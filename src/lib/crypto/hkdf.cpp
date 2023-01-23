b70f59d9e21f0d15be96c7eeb2174fecdb1a31ef
Johannes Roth <johannes.roth@mtg.de>
2023-01-23 11:47:20 +0100

    make HKDF test use generalized hkdf class; remove hkdf_common.cpp

--- src/lib/crypto/hkdf.cpp
+++ src/lib/crypto/hkdf.cpp
@@ -24,3 +24,44 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
+
+#include "config.h"
+#include "hkdf.hpp"
+
+#if defined(CRYPTO_BACKEND_BOTAN)
+#include "hkdf_botan.hpp"
+#endif
+#if defined(CRYPTO_BACKEND_OPENSSL)
+#error HKDF not implemented for OpenSSL Backend
+#endif
+
+namespace rnp {
+std::unique_ptr<Hkdf>
+Hkdf::create(pgp_hash_alg_t alg)
+{
+#if defined(CRYPTO_BACKEND_OPENSSL)
+#error HKDF not implemented for OpenSSL
+    // return Hash_OpenSSL::create(alg);
+#elif defined(CRYPTO_BACKEND_BOTAN)
+    return Hkdf_Botan::create(alg);
+#else
+#error "Crypto backend not specified"
+#endif
+}
+
+size_t
+Hkdf::size() const
+{
+    return size_;
+}
+
+pgp_hash_alg_t Hkdf::alg() const
+{
+    return hash_alg_;
+}
+
+Hkdf::~Hkdf()
+{
+}
+
+} // namespace rnp
