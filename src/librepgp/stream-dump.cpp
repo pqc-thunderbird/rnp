a8f4c9c97eb9a91a2a3fd700378c24502c9af030
Johannes Roth <johannes.roth@mtg.de>
2023-06-12 17:15:51 +0200

    add standalone v6 Ed25519 algorithm

--- src/librepgp/stream-dump.cpp
+++ src/librepgp/stream-dump.cpp
@@ -143,6 +143,9 @@ static const id_str_pair pubkey_alg_map[] = {
   {PGP_PKA_RESERVED_DH, "Reserved for DH (X9.42)"},
   {PGP_PKA_EDDSA, "EdDSA"},
   {PGP_PKA_SM2, "SM2"},
+#if defined(ENABLE_CRYPTO_REFRESH)
+  {PGP_PKA_ED25519, "Ed25519"},
+#endif
   {0x00, NULL},
 };
 
@@ -319,6 +322,20 @@ dst_print_mpi(pgp_dest_t *dst, const char *name, pgp_mpi_t *mpi, bool dumpbin)
         dst_printf(dst, "%s: %d bits, %s\n", name, (int) mpi_bits(mpi), hex);
     }
 }
+
+#if defined(ENABLE_CRYPTO_REFRESH)
+static void
+dst_print_vec(pgp_dest_t *dst, const char *name, std::vector<uint8_t> encoded, bool dumpbin)
+{
+    std::vector<char> hex(2*encoded.size());
+    if (!dumpbin) {
+        dst_printf(dst, "%s\n", name);
+    } else {
+        vsnprinthex(hex.data(), hex.size(), encoded.data(), encoded.size());
+        dst_printf(dst, "%s, %s\n", name, hex.data());
+    }
+}
+#endif
 
 static void
 dst_print_palg(pgp_dest_t *dst, const char *name, pgp_pubkey_alg_t palg)
@@ -780,6 +797,11 @@ stream_dump_signature_pkt(rnp_dump_ctx_t *ctx, pgp_signature_t *sig, pgp_dest_t 
         dst_print_mpi(dst, "eg r", &material.eg.r, ctx->dump_mpi);
         dst_print_mpi(dst, "eg s", &material.eg.s, ctx->dump_mpi);
         break;
+#if defined(ENABLE_CRYPTO_REFRESH)
+    case PGP_PKA_ED25519:
+        dst_print_vec(dst, "ed25519 sig", material.ed25519.sig, ctx->dump_mpi);
+        break;
+#endif
     default:
         dst_printf(dst, "unknown algorithm\n");
     }
@@ -874,6 +896,11 @@ stream_dump_key(rnp_dump_ctx_t *ctx, pgp_source_t *src, pgp_dest_t *dst)
         dst_printf(dst, "ecdh key wrap algorithm: %d\n", (int) key.material.ec.key_wrap_alg);
         break;
     }
+#if defined(ENABLE_CRYPTO_REFRESH)
+    case PGP_PKA_ED25519:
+        dst_print_vec(dst, "ed25519", key.material.ed25519.pub, ctx->dump_mpi);
+        break;
+#endif
     default:
         dst_printf(dst, "unknown public key algorithm\n");
     }
@@ -1896,6 +1923,11 @@ stream_dump_signature_pkt_json(rnp_dump_ctx_t *       ctx,
             goto done;
         }
         break;
+#if defined(ENABLE_CRYPTO_REFRESH)
+    case PGP_PKA_ED25519:
+        /* TODO */
+        break;
+#endif
     default:
         break;
     }
@@ -2020,6 +2052,11 @@ stream_dump_key_json(rnp_dump_ctx_t *ctx, pgp_source_t *src, json_object *pkt)
         }
         break;
     }
+#if defined(ENABLE_CRYPTO_REFRESH)
+    case PGP_PKA_ED25519:
+        /* TODO */
+        break;
+#endif
     default:
         break;
     }
@@ -2153,6 +2190,11 @@ stream_dump_pk_session_key_json(rnp_dump_ctx_t *ctx, pgp_source_t *src, json_obj
             return RNP_ERROR_OUT_OF_MEMORY;
         }
         break;
+#if defined(ENABLE_CRYPTO_REFRESH)
+    case PGP_PKA_ED25519:
+        /* TODO */
+        break;
+#endif
     default:;
     }
 
