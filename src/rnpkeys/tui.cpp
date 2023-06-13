037ff1b9cf3cf8b7f13a7c561b9501e5c69797f4
Johannes Roth <johannes.roth@mtg.de>
2023-06-13 10:59:52 +0200

    fix tui.cpp ifdefs

--- src/rnpkeys/tui.cpp
+++ src/rnpkeys/tui.cpp
@@ -210,8 +210,10 @@ rnpkeys_ask_generate_params(rnp_cfg &cfg, FILE *input_fp)
                "\t(21) EDDSA + ECDH (v6 key) \n"
 #endif
                "\t(22) EDDSA + ECDH (v4 key) \n"
-#if defined(ENABLE_PQC)
+#if defined(ENABLE_CRYPTO_REFRESH)
                "\t(23) ED25519 + X25519 (v6 key) \n"
+#endif
+#if defined(ENABLE_PQC)
                "\t(25) (Dilithium3 + Ed25519) + (Kyber768 + X25519)\n"
                //"\t(26) (Dilithium5 + Ed448) + (Kyber1024 + X448)\n"
                "\t(27) (Dilithium3 + ECDSA-NIST-P-256) + (Kyber768 + ECDH-NIST-P-256)\n"
@@ -273,13 +275,15 @@ rnpkeys_ask_generate_params(rnp_cfg &cfg, FILE *input_fp)
             cfg.set_str(CFG_KG_SUBKEY_CURVE, "Curve25519");
             break;
         }
-#if defined(ENABLE_PQC)
+#if defined(ENABLE_CRYPTO_REFRESH)
         case 23: {
             cfg.set_str(CFG_KG_PRIMARY_ALG, RNP_ALGNAME_ED25519);
             cfg.set_str(CFG_KG_SUBKEY_ALG, RNP_ALGNAME_X25519);
             cfg.set_str(CFG_KG_V6_KEY, "true");
             break;
         }
+#endif
+#if defined(ENABLE_PQC)
         case 25:
             cfg.set_str(CFG_KG_PRIMARY_ALG, RNP_ALGNAME_DILITHIUM3_ED25519);
             cfg.set_str(CFG_KG_SUBKEY_ALG, RNP_ALGNAME_KYBER768_X25519);
@@ -342,12 +346,14 @@ rnpkeys_ask_generate_params_subkey(rnp_cfg &cfg, FILE *input_fp)
                "\t(18) ECDH\n"
                "\t(19) ECDSA\n"
                "\t(22) EDDSA\n"
+#if defined(ENABLE_PQC)
                "\t(25) Kyber768 + X25519\n"
                "\t(26) Kyber1024 + X448\n"
                "\t(27) Kyber768 + ECDH-NIST-P-256\n"
                "\t(28) Kyber1024 + ECDH-NIST-P-384\n"
                "\t(29) Kyber768 + ECDH-brainpoolP256r1\n"
                "\t(30) Kyber1024 + ECDH-brainpoolP384r1\n"
+#endif
                "\t(99) SM2"
                "> ");
         if (!rnp_secure_get_long_from_fd(input_fp, option, false)) {
@@ -395,6 +401,7 @@ rnpkeys_ask_generate_params_subkey(rnp_cfg &cfg, FILE *input_fp)
             cfg.set_str(CFG_KG_SUBKEY_ALG, RNP_ALGNAME_EDDSA);
             break;
         }
+#if defined(ENABLE_PQC)
         case 25: 
             cfg.set_str(CFG_KG_SUBKEY_ALG, RNP_ALGNAME_KYBER768_X25519);
             break;
@@ -413,6 +420,7 @@ rnpkeys_ask_generate_params_subkey(rnp_cfg &cfg, FILE *input_fp)
         case 30: 
             cfg.set_str(CFG_KG_SUBKEY_ALG, RNP_ALGNAME_KYBER1024_BP384);
             break;
+#endif
         case 99: {
             cfg.set_str(CFG_KG_SUBKEY_ALG, RNP_ALGNAME_SM2);
             if (!cfg.has(CFG_KG_HASH)) {
